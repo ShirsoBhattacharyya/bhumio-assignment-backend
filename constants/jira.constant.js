3c1bc2e177e6b558d81741d2c736582baf182382
ShirsoBhattacharyya <101390681+ShirsoBhattacharyya@users.noreply.github.com>
2023-12-03 19:36:44 +0530

    Jira Constants changed

--- constants/jira.constant.js
+++ constants/jira.constant.js
@@ -1,6 +1,6 @@
 const JIRA_API_TOKEN =
-  "ATATT3xFfGF0ZWdupnsqjF3vhAdW0Kv8ODQy4yTtp2NqahOcvQIzro7erlt3m7LEPNYQzzJdL-iw_7ZQmwwoBLvto8i8mf7isT91QS7qEsIVLKRsoYmUHT8sqyYiTocw2jLlnKQE8zS0_2Ot1FwrUv85FoAtQ_5iVWTMG-7pB-lkBAnaPv5fRRU=F3914F70";
-const JIRA_EMAIL_ID = "ram101sing@gmail.com";
-const JIRA_BASE_URL = "https://apintegration.atlassian.net";
+  "ATATT3xFfGF0VRN5GOGtQBUhLd-rsSxzRz5swqhfkaIDSp6CKV1gOUyn7cx2KO8uFJenCYb5wp25Zk3_d8ubdWZwDF-vtqPMFRjJYmehu2yCimbtGUTNV_C9HvyFAe99oqvPfmJgscg5yco5_UhDCuZJHryF7XxYWokYNESyOj6veaM_e0eNFqY=ADAB96F5";
+const JIRA_EMAIL_ID = "shirso369@gmail.com";
+const JIRA_BASE_URL = "https://shirso369.atlassian.net";
 
 module.exports = { JIRA_API_TOKEN, JIRA_EMAIL_ID, JIRA_BASE_URL };
