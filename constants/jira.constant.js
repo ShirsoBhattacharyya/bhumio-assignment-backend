9975e2b0eb2183d88b8413c4ed59c8086d997885
ShirsoBhattacharyya <101390681+ShirsoBhattacharyya@users.noreply.github.com>
2023-12-04 10:51:11 +0530

    configuring dotenv

--- constants/jira.constant.js
+++ constants/jira.constant.js
@@ -1,5 +1,7 @@
-const JIRA_API_TOKEN =
-  "ATATT3xFfGF0ROh5-4v2k6g5YCCdQ35SPmKgjn9WBN8J_7dK4AvA9zFpsqtlWtyUM6r82RaSyKFlJIWgjWFyQgzuN4imY0Hug7PaG4_rrYJ2VaNA_qLBPjApQPnYNdy9q8Ohd1wXTgZrutNUH-E1bW96e3Zuq2Bpl3v0jEx-_WA3wfr2IZJUwKM=FBE46879";
+const dotenv = require("dotenv");
+dotenv.config();
+
+const JIRA_API_TOKEN = process.env.JIRA_API_TOKEN;
 const JIRA_EMAIL_ID = "shirso369@gmail.com";
 const JIRA_BASE_URL = "https://shirso369.atlassian.net";
 
